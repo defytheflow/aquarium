883dfc1fcc20f0783dbb3eda18941ae3273c2b17
Artyom Danilov <defytheflow@gmail.com>
2020-05-10 22:24:33 +0300

    Refactor code
    Remove unnecessary Region enum class.

--- aquarium.py
+++ aquarium.py
@@ -3,28 +3,17 @@
 import os
 import enum
 import random
-from typing import Tuple
+import typing
 import tkinter as tk
-
-
-class Asset:
-
-    DIRECTORY = 'assets'
-
-    @classmethod
-    def path(cls, file: str) -> str:
-        return os.path.join(cls.DIRECTORY, file)
 
 
 class Window(tk.Tk):
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-
-        self._bg = tk.PhotoImage(file=Asset.path('bg.png'))
+        self._bg = tk.PhotoImage(file=os.path.join('assets', 'bg.png'))
         self._width = 800
         self._height = 600
-
         self.title('Aquarium')
         self.resizable(0, 0)
         self.geometry(f'{self._width}x{self._height}')
@@ -39,80 +28,34 @@ class Window(tk.Tk):
         self.mainloop()
 
 
-class CustomEnum(enum.Enum):
-
-    def __str__(self):
-        return self.name.lower()
-
-    @classmethod
-    def random(cls, exclude: 'CustomEnum' = None):
-        choices = set(list(cls)) - set([exclude]) if exclude else list(cls)
-        return random.choice(list(choices))
-
-
 @enum.unique
-class Region(CustomEnum):
-
-    TOP_LEFT = enum.auto()
-    TOP_CENTER = enum.auto()
-    TOP_RIGHT = enum.auto()
-
-    MIDDLE_LEFT = enum.auto()
-    MIDDLE_CENTER = enum.auto()
-    MIDDLE_RIGHT = enum.auto()
-
-    BOTTOM_LEFT = enum.auto()
-    BOTTOM_CENTER = enum.auto()
-    BOTTOM_RIGHT = enum.auto()
-
-    def x_part(self) -> str:
-        ''' Returns LEFT, CENTER, or RIGHT. '''
-        return self.name.split('_')[1]
-
-    def y_part(self) -> str:
-        ''' Return TOP, MIDDLE, or BOTTOM. '''
-        return self.name.split('_')[0]
-
-    def rand_coords(self, width, height) -> Tuple[int, int]:
-        ''' Return random coordinates for this region. '''
-        x_step = width // 3
-        y_step = height // 3
-
-        ranges = {
-            'LEFT':   (0, x_step),
-            'CENTER': (x_step, x_step * 2),
-            'RIGHT':  (x_step * 2, x_step * 3),
-            'TOP':    (0, y_step),
-            'MIDDLE': (y_step, y_step * 2),
-            'BOTTOM': (y_step * 2, y_step * 3),
-        }
-
-        x_range = ranges[self.x_part()]
-        y_range = ranges[self.y_part()]
-
-        return (random.randint(*x_range), random.randint(*y_range))
-
-
-@enum.unique
-class Direction(CustomEnum):
+class Direction(enum.Enum):
+    """ Represents fish direction. """
 
     WEST = enum.auto()
     EAST = enum.auto()
     NORTH = enum.auto()
     SOUTH = enum.auto()
 
+    def __str__(self):
+        return self.name.lower()
+
     @classmethod
-    def hrandom(cls) -> 'Direction':
-        ''' h - horizontal. '''
+    def random(cls, exclude: 'Direction' = None):
+        choices = set(list(cls)) - {exclude} if exclude else list(cls)
+        return random.choice(list(choices))
+
+    @classmethod
+    def random_horizontal(cls) -> 'Direction':
         return random.choice([cls.WEST, cls.EAST])
 
     @classmethod
-    def vrandom(cls) -> 'Direction':
-        ''' v -vertical. '''
+    def random_vertical(cls) -> 'Direction':
         return random.choice([cls.NORTH, cls.SOUTH])
 
 
 class BorderCollisionError(Exception):
+    """ Thrown when fish stumbles open a border. """
     pass
 
 
@@ -120,30 +63,22 @@ class Fish:
 
     def __init__(self, canvas):
         self._canvas = canvas
-        self._region = Region.random()
         self._direction = Direction.random()
-        self._velocity = 5
-        self._update_time = 100
-        file = Asset.path(f'fish-{self._direction}.png')
-        self._image = tk.PhotoImage(file=file)
+        self._image = tk.PhotoImage(file=os.path.join('assets', f'fish-{self._direction}.png'))
         self._x_max = self._canvas.winfo_reqwidth() - self._image.width()
         self._y_max = self._canvas.winfo_reqheight() - self._image.height()
-        self._x, self._y = self._region.rand_coords(self._x_max, self._y_max)
-        self._id = self._canvas.create_image(
-            self._x, self._y, image=self._image, anchor=tk.NW
-        )
+        self._x, self._y = self.get_random_coordinates(self._x_max, self._y_max)
+        self._velocity = 5
+        self._update_time = 100
+        self._id = self._canvas.create_image(self._x, self._y, image=self._image, anchor=tk.NW)
 
     @property
     def image(self) -> tk.PhotoImage:
         return self._image
 
     @property
-    def direction(self) -> 'Direction':
+    def direction(self) -> Direction:
         return self._direction
-
-    @property
-    def region(self) -> 'Region':
-        return self._region
 
     @property
     def x(self) -> int:
@@ -157,33 +92,27 @@ class Fish:
     def update_time(self) -> int:
         return self._update_time
 
-    @image.setter
-    def image(self, file: str):
-        ''' Change fish image on the canvas. '''
+    def set_image(self, file: str):
+        """ Change fish image on the canvas. """
         self._image = tk.PhotoImage(file=file)
         self._canvas.itemconfig(self._id, image=self._image)
 
-    @direction.setter
-    def direction(self, direction: 'Direction'):
-        ''' Change fish direction and it's image respectively. '''
+    def set_direction(self, direction: Direction):
+        """ Change fish direction and it's image accordingly. """
         self._direction = direction
-        self.image = Asset.path(f'fish-{self._direction}.png')
+        self.set_image(os.path.join('assets', f'fish-{self._direction}.png'))
 
     def swim(self):
-        '''  '''
+        """ """
         try:
             self.swim_forward()
         except BorderCollisionError:
-            self.direction = Direction.random(exclude=self._direction)
+            self.set_direction(Direction.random(exclude=self._direction))
 
         self._canvas.after(self._update_time, self.swim)
 
-    def _update_position(self):
-        self._x, self._y = self._canvas.coords(self._id)
-
     def swim_forward(self):
-        '''  '''
-        self._update_position()
+        self._update_coordinates()
 
         border_collision = {
             Direction.WEST:  self._x < 0,
@@ -204,6 +133,13 @@ class Fish:
 
         off_x, off_y = offset[self._direction]
         self._canvas.move(self._id, off_x, off_y)
+
+    def _update_coordinates(self):
+        self._x, self._y = self._canvas.coords(self._id)
+
+    @staticmethod
+    def get_random_coordinates(width: int, height: int) -> typing.Tuple[int, int]:
+        return random.randint(0, width), random.randint(0, height)
 
 
 if __name__ == '__main__':
--- test_aquarium.py
+++ test_aquarium.py
@@ -1,34 +1,8 @@
 #!/usr/bin/python3
 
-import os
 import unittest
 
-from aquarium import Asset, Region, Direction
-
-
-class TestAssetMethods(unittest.TestCase):
-
-    def test_path(self):
-        correct = os.path.join('assets', 'sea.png')
-        self.assertEqual(Asset.path('sea.png'), correct)
-
-
-class TestRegionMethods(unittest.TestCase):
-
-    def test_random(self):
-        self.assertIn(Region.random(), list(Region))
-
-    def test_x_part(self):
-        self.assertEqual(Region.TOP_LEFT.x_part(), 'LEFT')
-
-    def test_y_part(self):
-        self.assertEqual(Region.TOP_LEFT.y_part(), 'TOP')
-
-    def test_rand_coords(self):
-        x1, y1 = Region.TOP_LEFT.rand_coords(300, 300)
-        self.assertTrue(0 <= x1 <= 100 and 0 <= y1 <= 100)
-        x2, y2 = Region.BOTTOM_RIGHT.rand_coords(600, 600)
-        self.assertTrue(400 < x2 <= 600 and 400 < y2 <= 600)
+from aquarium import Direction
 
 
 class TestDirectionMethods(unittest.TestCase):
@@ -41,11 +15,11 @@ class TestDirectionMethods(unittest.TestCase):
         direction = Direction.random(exclude=exclude)
         self.assertNotEqual(direction, exclude)
 
-    def test_hrandom(self):
-        self.assertIn(Direction.hrandom(), [Direction.WEST, Direction.EAST])
+    def test_random_horizontal(self):
+        self.assertIn(Direction.random_horizontal(), [Direction.WEST, Direction.EAST])
 
-    def test_vrandom(self):
-        self.assertIn(Direction.vrandom(), [Direction.NORTH, Direction.SOUTH])
+    def test_random_vertical(self):
+        self.assertIn(Direction.random_vertical(), [Direction.NORTH, Direction.SOUTH])
 
 
 if __name__ == '__main__':
