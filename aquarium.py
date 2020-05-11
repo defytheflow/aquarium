1930636ac18dda73710f703425166cd069d05740
Artyom Danilov <defytheflow@gmail.com>
2020-05-11 15:39:02 +0300

    Add new images
    Now when fish collides with west or east borders and decided to go north or
    south, it chooses a different image depending on the xregion (left or right)

--- aquarium.py
+++ aquarium.py
@@ -46,6 +46,10 @@ class Direction(enum.Enum):
         return self in [self.NORTH, self.SOUTH]
 
     @classmethod
+    def random_horizontal(cls):
+        return random.choice([cls.WEST, cls.EAST])
+
+    @classmethod
     def random(cls, exclude: list = None):
         choices = set(list(cls)) - set(exclude) if exclude else list(cls)
         return random.choice(list(choices))
@@ -80,24 +84,42 @@ class Fish:
 
     def __init__(self, canvas: tk.Canvas):
         self._canvas = canvas
-        self._direction = Direction.random()
-        self._image = tk.PhotoImage(file=os.path.join('assets', f'fish-{self._direction}.png'))
-        self._x_max = self._canvas.winfo_reqwidth() - self._image.width()
-        self._y_max = self._canvas.winfo_reqheight() - self._image.height()
-        self._x, self._y = self.get_random_coordinates(self._x_max, self._y_max)
+        self._direction = Direction.random_horizontal()
+        self._prev_direction = None
+        self._image = tk.PhotoImage(file=self._get_image_file())
+        self._x_bound, self._y_bound = self._get_xy_bounds()
+        self._x, self._y = self.get_random_coordinates(self._x_bound, self._y_bound)
         self._id = self._canvas.create_image(self._x, self._y, image=self._image, anchor=tk.NW)
-        self._x_region = self._get_x_region()
-        self._y_region = self._get_y_region()
+        self._x_region, self._y_region = self._get_xy_regions()
         self._velocity = 5
         self._update_time = 100
 
-    def _change_image(self, file: str):
+    def _get_image_file(self) -> str:
+        """ Returns name of the image file depending on current direction and x_region. """
+        if self._direction.is_vertical():
+            file = f'fish-{self._direction}-{self._x_region}.png'
+        else:
+            file = f'fish-{self._direction}.png'
+        return os.path.join('assets', file)
+
+    def _change_image(self):
         """ Change fish image on the canvas. """
-        self._image = tk.PhotoImage(file=file)
-        self._canvas.itemconfig(self._id, image=self._image)
+        self._canvas.delete(self._id)
+        self._image = tk.PhotoImage(file=self._get_image_file())
+        self._x_bound, self._y_bound = self._get_xy_bounds()
+        if self._prev_direction is Direction.WEST:
+            self._x = 0
+        elif self._prev_direction is Direction.EAST:
+            self._x = self._x_bound
+        elif self._prev_direction is Direction.NORTH:
+            self._y = 0
+        else:
+            self._y = self._y_bound
+        self._id = self._canvas.create_image(self._x, self._y, image=self._image, anchor=tk.NW)
 
     def _change_direction(self):
         """ Change fish direction and it's image accordingly. """
+        self._prev_direction = self._direction
         if self._direction.is_horizontal():
             if self._y_region is YRegion.TOP:
                 self._direction = Direction.random(exclude=[self._direction, Direction.NORTH])
@@ -112,7 +134,7 @@ class Fish:
                 self._direction = Direction.random(exclude=[self._direction])
             else:
                 self._direction = Direction.random(exclude=[self._direction, Direction.EAST])
-        self._change_image(os.path.join('assets', f'fish-{self._direction}.png'))
+        self._change_image()
 
     def swim(self):
         """ """
@@ -143,18 +165,20 @@ class Fish:
     def _update_position(self):
         """ Update current x, y coordinates and regions. """
         self._x, self._y = self._canvas.coords(self._id)
-        self._x_region, self._y_region = self._get_x_region(), self._get_y_region()
+        self._x_region, self._y_region = self._get_xy_regions()
+        # print(f'x: {self._x}, y: {self._y}')
+        # print(f'x_max: {self._x_bound}, y_max: {self._y_bound}')
 
     def _collides_with_border(self) -> bool:
         """ Check if fish collides with a border in current direction. """
         if self._direction is Direction.WEST:
             return self._x < 0
         if self._direction is Direction.EAST:
-            return self._x > self._x_max
+            return self._x > self._x_bound
         if self._direction is Direction.NORTH:
             return self._y < 0
         if self._direction is Direction.SOUTH:
-            return self._y > self._y_max
+            return self._y > self._y_bound
 
     def _bounce_back(self):
         """ Bounce back after colliding with a border. """
@@ -166,22 +190,28 @@ class Fish:
             self._canvas.move(self._id, 0, self._velocity)
         elif self._direction is Direction.SOUTH:
             self._canvas.move(self._id, 0, -self._velocity)
+        self._update_position()
 
-    def _get_x_region(self) -> XRegion:
-        if 0 <= self._x <= self._x_max // 3:
-            return XRegion.LEFT
-        if self._x_max // 3 <= self._x <= self._x_max // 3 * 2:
-            return XRegion.CENTER
-        if self._x_max // 3 * 2 <= self._x <= self._x_max:
-            return XRegion.RIGHT
+    def _get_xy_bounds(self) -> typing.Tuple[int, int]:
+        return self._canvas.winfo_reqwidth() - self._image.width(), \
+               self._canvas.winfo_reqheight() - self._image.height()
 
-    def _get_y_region(self) -> YRegion:
-        if 0 <= self._y <= self._y_max // 3:
-            return YRegion.TOP
-        if self._y_max // 3 <= self._y <= self._y_max // 3 * 2:
-            return YRegion.MIDDLE
-        if self._y_max // 3 * 2 <= self._y <= self._y_max:
-            return YRegion.BOTTOM
+    def _get_xy_regions(self) -> typing.Tuple[XRegion, YRegion]:
+        if 0 <= self._x <= self._x_bound // 3:
+            x_region = XRegion.LEFT
+        elif self._x_bound // 3 <= self._x <= self._x_bound // 3 * 2:
+            x_region = XRegion.CENTER
+        else:
+            x_region = XRegion.RIGHT
+
+        if 0 <= self._y <= self._y_bound // 3:
+            y_region = YRegion.TOP
+        elif self._y_bound // 3 <= self._y <= self._y_bound // 3 * 2:
+            y_region = YRegion.MIDDLE
+        else:
+            y_region = YRegion.BOTTOM
+
+        return x_region, y_region
 
     @staticmethod
     def get_random_coordinates(width: int, height: int) -> typing.Tuple[int, int]:
