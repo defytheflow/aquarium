59352810f4b85ae6eb060cd6279d0852c06bd14a
Artyom Danilov <defytheflow@gmail.com>
2020-05-12 19:51:37 +0300

    Create a Canvas and Menu classes. Make fish change direction when clicked by mouse.
    Type: feat

--- aquarium.py
+++ aquarium.py
@@ -9,6 +9,91 @@ from typing import Tuple, List
 from PIL import ImageTk, Image
 
 
+class Window(tk.Tk):
+
+    BG_IMAGES = [
+        os.path.join('assets', 'bg', file) for file in os.listdir(os.path.join('assets', 'bg'))
+    ]
+
+    def __init__(self, width: int, height: int, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Menu
+        self.menu = Menu(self)
+        self.config(menu=Menu(self))
+        # Canvas
+        self.canvas = Canvas(self, width=width, height=height)
+        self.canvas.set_bg_image(random.choice(self.BG_IMAGES))
+        self.canvas.pack(expand=tk.YES, fill=tk.BOTH)
+        # Self
+        self.title('Aquarium')
+        self.resizable(0, 0)
+        self.geometry(f'{width}x{height}')
+        self.mainloop()
+
+
+class Menu(tk.Menu):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self._view_menu = tk.Menu(self._root(), tearoff=0)
+
+        self._view_menu.add_cascade(label='Fish', menu=self._create_fish_menu())
+        self._view_menu.add_cascade(label='Background', menu=self._create_bg_menu())
+
+        self.add_cascade(label='View', menu=self._view_menu)
+
+    def _create_fish_menu(self) -> tk.Menu:
+        menu = tk.Menu(self, tearoff=0)
+        var = tk.StringVar()
+        for sprite_name in Fish.SPRITE_NAMES:
+            menu.add_radiobutton(
+                label=sprite_name.capitalize(),
+                variable=var,
+                value=sprite_name,
+                command=lambda: self._root().canvas.fish.set_sprite_name(var.get())
+            )
+        return menu
+
+    def _create_bg_menu(self) -> tk.Menu:
+        menu = tk.Menu(self, tearoff=0)
+        var = tk.StringVar()
+        for image_path in self._root().BG_IMAGES:
+            _, tail = os.path.split(image_path)
+            menu.add_radiobutton(
+                label=tail.capitalize()[:-4],
+                variable=var,
+                value=image_path,
+                command=lambda: self._root().canvas.set_bg_image(var.get())
+            )
+        return menu
+
+
+class Canvas(tk.Canvas):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Background Image
+        self._bg_image = None
+        self._bg_image_id = None
+        # Fish
+        self.fish = Fish(self, 'blue')
+        self.fish.swim()
+
+        self.bind('<Button-1>', self._handle_left_click)
+
+    def set_bg_image(self, file_name: str):
+        if self._bg_image_id:
+            self.delete(self._bg_image_id)
+        self._bg_image = Utils.load_image(file_name)
+        self._bg_image_id = self.create_image(0, 0, image=self._bg_image, anchor=tk.NW)
+        self.fish.repaint()
+
+    def _handle_left_click(self, event):
+        if self.fish.check_collision(event.x, event.y):
+            self.fish.change_direction()
+
+
 class Utils:
 
     @staticmethod
@@ -22,68 +107,6 @@ class Utils:
     @staticmethod
     def get_random_xy(x_range: Tuple[int, int], y_range: Tuple[int, int]) -> Tuple[int, int]:
         return random.randint(*x_range), random.randint(*y_range)
-
-
-class Window(tk.Tk):
-
-    BG_IMAGES = os.listdir(os.path.join('assets', 'bg'))
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._width = 800
-        self._height = 600
-
-        self._canvas = tk.Canvas(self, width=self._width, height=self._height)
-        self._canvas.pack(expand=tk.YES, fill=tk.BOTH)
-
-        self._fish = Fish(self._canvas, 'blue')
-        self._fish.swim()
-
-        self._bg = None
-        self._bg_id = None
-        self.set_bg(random.choice(self.BG_IMAGES))
-
-        self.title('Aquarium')
-        self.resizable(0, 0)
-        self.geometry(f'{self._width}x{self._height}')
-
-        self.menu = tk.Menu(self)
-        self.menu.add_cascade(label='Fish', menu=self._create_fish_menu())
-        self.menu.add_cascade(label='Background', menu=self._create_bg_menu())
-        self.config(menu=self.menu)
-
-        self.mainloop()
-
-    def set_bg(self, file: str):
-        if self._bg:
-            self._canvas.delete(self._bg_id)
-        self._bg = Utils.load_image(os.path.join('assets', 'bg', file))
-        self._bg_id = self._canvas.create_image(0, 0, image=self._bg, anchor=tk.NW)
-        self._fish.repaint()
-
-    def _create_bg_menu(self) -> tk.Menu:
-        menu = tk.Menu(self.menu, tearoff=0)
-        var = tk.StringVar()
-        for image_name in self.BG_IMAGES:
-            menu.add_radiobutton(
-                label=image_name.capitalize()[:-4],
-                variable=var,
-                value=image_name,
-                command=lambda: self.set_bg(var.get())
-            )
-        return menu
-
-    def _create_fish_menu(self) -> tk.Menu:
-        menu = tk.Menu(self.menu, tearoff=0)
-        var = tk.StringVar()
-        for sprite_name in Fish.SPRITE_NAMES:
-            menu.add_radiobutton(
-                 label=sprite_name.capitalize(),
-                 variable=var,
-                 value=sprite_name,
-                 command=lambda: self._fish.set_sprite_name(var.get())
-            )
-        return menu
 
 
 @enum.unique
@@ -179,16 +202,45 @@ class Fish:
         self._sprite_name = new_sprite_name
         self.repaint()
 
+    def change_direction(self):
+        """ """
+        self._prev_direction = self._direction
+        exclude_directions: List[Direction] = [self._direction]
+        if self._direction.is_horizontal():
+            if self._region[1] is Region.TOP:
+                exclude_directions.append(Direction.NORTH)
+            elif self._region[1] is Region.BOTTOM:
+                exclude_directions.append(Direction.SOUTH)
+        else:
+            if self._region[0] is Region.LEFT:
+                exclude_directions.append(Direction.WEST)
+            elif self._region[0] is Region.RIGHT:
+                exclude_directions.append(Direction.EAST)
+        print(f'exclude_directions: {exclude_directions}')
+        self._direction = Direction.random(exclude=exclude_directions)
+        print(f'direction is {self._direction}')
+        self.repaint()
+
+    def check_collision(self, x: int, y: int) -> bool:
+        """ """
+        return self._x <= x <= self._x + self._image.width() and self._y <= y <= self._y + self._image.height()
+
     def repaint(self):
         """ """
         self._canvas.delete(self._image_id)
-        self._image = Utils.load_image(self._get_image_path())
+
+        new_image = Utils.load_image(self._get_image_path())
+        if self._prev_direction is Direction.EAST:
+            self._x += self._image.width() - new_image.width()
+        elif self._prev_direction is Direction.SOUTH:
+            self._y += self._image.height() - new_image.height()
+        self._image = new_image
+
         self._x_max, self._y_max = Utils.get_image_max_xy(self._canvas, self._image)
         self._image_id = self._canvas.create_image(self._x, self._y, image=self._image, anchor=tk.NW)
 
     def swim(self):
         """ """
-
         try:
             self._move_forward()
         except BorderCollisionError:
@@ -215,7 +267,6 @@ class Fish:
 
     def _move_backward(self):
         """ Move toward opposite direction. """
-
         if self._direction is Direction.WEST:
             x_move, y_move = self._velocity, 0
         elif self._direction is Direction.EAST:
@@ -234,30 +285,10 @@ class Fish:
 
     def _handle_border_collision(self):
         self._move_backward()
-        self._change_direction()
-
-    def _change_direction(self):
-        """ """
-        self._prev_direction = self._direction
-        exclude_directions: List[Direction] = [self._direction]
-
-        if self._direction.is_horizontal():
-            if self._region[1] is Region.TOP:
-                exclude_directions.append(Direction.NORTH)
-            elif self._region[1] is Region.BOTTOM:
-                exclude_directions.append(Direction.SOUTH)
-        else:
-            if self._region[0] is Region.LEFT:
-                exclude_directions.append(Direction.WEST)
-            elif self._region[0] is Region.RIGHT:
-                exclude_directions.append(Direction.EAST)
-
-        self._direction = Direction.random(exclude=exclude_directions)
-        self.repaint()
+        self.change_direction()
 
     def _check_border_collision(self):
         """ Raises BorderCollisionError if fish collides with a border. """
-
         if self._direction is Direction.WEST:
             collision_condition = self._x < 0
         elif self._direction is Direction.EAST:
@@ -273,11 +304,13 @@ class Fish:
     def _get_image_path(self) -> str:
         """ Returns the path to the image file depending on the current direction and x region. """
         if self._direction.is_horizontal():
-            file = f'{self._direction}.png'
+            file = f"{self._direction}.png"
+        elif self._region[0] is Region.CENTER:
+            file = f"{self._direction}-{random.choice(['left', 'right'])}.png"
         else:
-            file = f'{self._direction}-{self._region[0]}.png'
+            file = f"{self._direction}-{self._region[0]}.png"
         return os.path.join('assets', 'fish', self._sprite_name, file)
 
 
 if __name__ == '__main__':
-    app = Window()
+    app = Window(800, 600)
